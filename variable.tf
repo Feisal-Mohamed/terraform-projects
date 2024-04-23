33c6f4e5567cd80eeee50f028ada89996b4d8e27
Feisal Mohamed <feisal_mohamed@hotmail.com>
2024-04-23 01:50:17 +0100

    created rds file

--- variable.tf
+++ variable.tf
@@ -47,3 +47,28 @@ variable "ssh_location" {
     description     = "the ip address that can ssh into an ec2 instance"
     type            = string
 }
+
+# rds variables
+variable "database_snapshot_identifier" {
+    default         = "arn:aws:rds:us-east-1:669062246420:snapshot:fleetcart-final-snapshot"
+    description     = "the database snapshot arn"
+    type            = string
+}
+
+variable "database_instance_class" {
+    default         = "db.t3.micro"
+    description     = "the database snapshot arn"
+    type            = string
+}
+
+variable "database_instance_identifier" {
+    default         = "dev-rds-db"
+    description     = "the database instance identifier"
+    type            = string
+}
+
+variable "multi_az_deployment" {
+    default         = false
+    description     = "create a standby db instance"
+    type            = bool
+}
